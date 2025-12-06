f963eff3b0f78995f424b05bc49d8bafe2fe421c
VinX-To-play <v@lundborgs.de>
2025-12-06 19:15:47 +0100

    added back vincent hosts

--- nixos/module.nix
+++ nixos/module.nix
@@ -4,6 +4,15 @@ with lib;
 
 let
   cfg = config.services.sheardHosts;  # consistent name
+  hosts = {
+    "100.64.0.11" = [
+      "komga.slave.int"
+      "vaultwarden.slave.int"
+      "slave.int"
+      "ca.slave.int"
+    ];
+  };
+
 in {
   options.services.sheardHosts = {
     enable = mkOption {
@@ -11,20 +20,9 @@ in {
       default = false;
       description = "Enable sheardHosts module.";
     };
-
-    extraHosts = mkOption {
-      type = types.attrsOf (types.listOf types.str);
-      default = { };
-      description = ''
-        Map of IP -> list of hostnames to add to /etc/hosts.
-        Example: { "10.0.0.5" = [ "myhost" "myhost.local" ]; }
-      '';
-    };
   };
 
   config = mkIf cfg.enable {
-    networking.extraHosts = lib.concatMapAttrs (ip: names:
-      builtins.concatStringsSep "\n" (map (n: "${ip} ${n}") names)
-    ) cfg.extraHosts;
+    networking.extraHosts = hosts;
   };
 }
